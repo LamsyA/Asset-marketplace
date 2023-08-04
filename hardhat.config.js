caca9e42da9cf47f9ffc30c28108fd0a70d26006
LamsyA <olamide.adetula.1744@gmail.com>
2023-08-04 16:22:45 +0100

    Fixing Deployment Error

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
-const { API_URL, PRIVATE_KEY , CELO_URL} = process.env;
+const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.17",
   paths: {
@@ -17,7 +17,7 @@ module.exports = {
     // },
    
     Alfajores:{
-      url: CELO_URL,
+      url: "https://alfajores-forno.celo-testnet.org",
       accounts: [`0x${PRIVATE_KEY}`],
     chainId: 44787
 
